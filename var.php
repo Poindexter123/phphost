527bedbf2b580f4cbaa43e203772cc8625ad127d
Poindexter123 <124400565+Poindexter123@users.noreply.github.com>
2023-03-21 22:42:49 +0530

    Add files via upload

---
+++ var.php
@@ -0,0 +1,17 @@
+<?php
+    $name="John Smith";
+    $age=56;
+    function display(){
+    global $name,$age;
+    echo "My name is $name and my age is $age";
+    }
+    display();
+    function display1(){
+        global $name,$age;
+        $GLOBALS['name']="BlackAdam";
+        echo "<br>" .$GLOBALS['name']."<br>";
+    }
+    display1();
+    display();
+    
+    ?>
